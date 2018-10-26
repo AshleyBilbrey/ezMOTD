cd7f704eb9efe8aac2550da290d1471b71cff77f
Ashley Bilbrey <ashe.bilbrey@gmail.com>
2018-10-25 20:04:45 -0600

    Start EzMOTD plugin

---
+++ src/com/ashleybilbrey/ezMOTD/EzMOTD.java
@@ -0,0 +1,12 @@
+package com.ashleybilbrey.ezMOTD;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EzMOTD extends JavaPlugin {
+
+    public void onEnable() {
+        getLogger().info("ezMOTD Loaded");
+        getLogger().info("ezMOTD by Ashley Bilbrey");
+    }
+
+}
